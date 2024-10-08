8144106f53a1231f0aa3462eab6724174ef0099b
Mihai Racoreanu <mihai@bodyswaps.co>
2024-10-08 01:57:51 +0100

    a few theming options

--- app.vue
+++ app.vue
@@ -1,19 +1,20 @@
 <script setup>
-const { setLocale } = useI18n()
+  const { setLocale } = useI18n()
 </script>
 
 <template>
-  <div>
-    <button @click="setLocale('en')">en</button>
-    <button @click="setLocale('fr')">fr</button>
+  <div class="bg-stone-200 dark:bg-stone-950 min-h-screen w-full">
+    <UButton @click="setLocale('en')">en</UButton>
+    <UButton @click="setLocale('fr')">fr</UButton>
+    <UCard>
+      <template #header>
+        <div class="h-8">{{ $t('welcome') }}</div>
+      </template>
+      <div class="h-16">{{ $t('message') }}</div>
+      <template #footer>
+        <div class="h-8">{{ $t('greeting.hello') }}</div>
+      </template>
+    </UCard>
+    <UMeter :value="80" indicator label="Memory usage" />
   </div>
-  <UCard>
-    <template #header>
-      <div class="h-8">{{ $t('welcome') }}</div>
-    </template>
-    <div class="h-16">{{ $t('message') }}</div>
-    <template #footer>
-      <div class="h-8">{{ $t('greeting.hello') }}</div>
-    </template>
-  </UCard>
 </template>
